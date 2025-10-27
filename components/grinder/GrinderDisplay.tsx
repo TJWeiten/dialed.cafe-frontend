3fced242aa28bb0ce9b3c55703e833d88e8cc1bf
T.J. Weiten <me@tjweiten.com>
2025-10-26 23:54:43 -0700

    Fixed deprecated prop

--- components/grinder/GrinderDisplay.tsx
+++ components/grinder/GrinderDisplay.tsx
@@ -3,21 +3,13 @@ import { Grinder } from "@/types/grinder";
 
 interface GrinderDisplayProps {
     grinders: Grinder[];
-    onRefetch: () => void;
 }
 
-export default function GrinderDisplay({
-    grinders,
-    onRefetch,
-}: GrinderDisplayProps) {
+export default function GrinderDisplay({ grinders }: GrinderDisplayProps) {
     return (
         <div className="grid auto-rows-max grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {grinders.map((grinder) => (
-                <GrinderCard
-                    key={grinder.id}
-                    grinder={grinder}
-                    onRefetch={onRefetch}
-                />
+                <GrinderCard key={grinder.id} grinder={grinder} />
             ))}
         </div>
     );
